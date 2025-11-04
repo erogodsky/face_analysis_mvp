f4dab56489443a8690f5eaf618e0c9000cd65923
erogodsky <erogodsky93@gmail.com>
2025-11-05 02:39:40 +0300

    lipreading with auto_avsr

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
 import cv2, json
+import argparse
+
+from modules.face_detection import FaceDetector
 from modules.face_recognition_module import FaceIdentifier
 from modules.landmarks_pose import LandmarkPoseEstimator
-# from modules.lipreading import LipReader
+from modules.lipreading import LipReader
+
+
+parser = argparse.ArgumentParser(description="Face analysis MVP")
+parser.add_argument(
+    "--input",
+    type=str,
+    default='data/input.mp4',
+    help="Input source: path to video file (default: 'data/input.mp4')."
+)
+args = parser.parse_args()
 
 
 def main():
     cap = cv2.VideoCapture("data/input.mp4")
+    face_detector = FaceDetector()
     face_id = FaceIdentifier("gallery")
     pose_estimator = LandmarkPoseEstimator()
-    # lip_reader = LipReader()
+    lip_reader = LipReader()
 
     frames_data = []
+    frames = []
+    landmark_list = []
 
     while True:
         ret, frame = cap.read()
@@ -18,20 +34,22 @@ def main():
             break
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        identities = face_id.identify(frame)
+        face_bbox = face_detector.detect(frame)
+        identity = face_id.identify(frame, face_bbox)
         landmarks, head_pose = pose_estimator.process_frame(frame)
-        # lip_reader.add_frame(frame, landmarks)
+        landmark_list.append(landmarks)
+        frames.append(frame)
 
         frames_data.append({
-            "identity": identities[0][0] if identities else "unknown",
+            "identity": identity,
             "landmarks": landmarks,
             "head_pose": head_pose
         })
 
-    # lip_text = lip_reader.predict(lip_frames)
+    lip_text = lip_reader.predict(frames, landmark_list)
 
-    # output = {"frames": frames_data, "lip_text": lip_text}
-    # json.dump(output, open("data/output.json", "w"), indent=2)
+    output = {"frames": frames_data, "lip_text": lip_text}
+    json.dump(output, open("data/output.json", "w"), indent=2)
 
 
 if __name__ == "__main__":
---
+++ modules/lipreading.py
@@ -0,0 +1,70 @@
+import numpy as np
+import torch
+import torchvision
+import argparse
+
+from auto_avsr.lightning import ModelModule
+from auto_avsr.preparation.detectors.mediapipe.detector import LandmarksDetector
+from auto_avsr.preparation.detectors.mediapipe.video_process import VideoProcess
+from auto_avsr.datamodule.transforms import VideoTransform
+
+
+CKPT_PATH = "models/vsr_trlrs3_base.pth"
+
+
+def preprocess_landmarks(shape, frames_lms):
+    h, w, _ = shape
+    landmarks = []
+    for lms in frames_lms:
+        keypoints = [
+            lms[468][:2],  # Left eye
+            lms[473][:2],  # Right eye
+            lms[1][:2],  # Nose tip
+            (np.array(lms[13][:2]) + np.array(lms[14][:2])) / 2  # Mouth center
+        ]
+        keypoints = np.array([[x*w, y*h] for x, y in keypoints])
+        landmarks.append(keypoints)
+    return landmarks
+
+
+class InferencePipeline(torch.nn.Module):
+    def __init__(self, ckpt_path):
+        super(InferencePipeline, self).__init__()
+
+        self.landmarks_detector = LandmarksDetector()
+        self.video_process = VideoProcess(convert_gray=False)
+        self.video_transform = VideoTransform(subset="test")
+
+        ckpt = torch.load(ckpt_path, map_location=lambda storage, loc: storage)
+        parser = argparse.ArgumentParser()
+        args, _ = parser.parse_known_args(args=[])
+        setattr(args, 'modality', 'video')
+        self.modelmodule = ModelModule(args)
+        self.modelmodule.model.load_state_dict(ckpt)
+        self.modelmodule.eval()
+
+    def load_video(self, data_filename):
+        return torchvision.io.read_video(data_filename, pts_unit="sec")[0].numpy()
+
+    def forward(self, frames, landmarks):
+        video = np.stack(frames, axis=0)
+        # landmarks = self.landmarks_detector(video)
+        keypoints = preprocess_landmarks(frames[0].shape, landmarks)
+        video = self.video_process(video, keypoints)
+        video = torch.tensor(video)
+        video = video.permute((0, 3, 1, 2))
+        video = self.video_transform(video)
+        with torch.no_grad():
+            transcript = self.modelmodule(video)
+
+        return transcript
+
+
+class LipReader:
+    def __init__(self):
+        self.pipeline = InferencePipeline(CKPT_PATH)
+
+
+    def predict(self, frames, landmarks):
+        transcript = self.pipeline(frames, landmarks)
+        return transcript
